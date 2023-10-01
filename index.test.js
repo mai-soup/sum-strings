c045e9b9b127a68a2cc84402199d25b034ab5e6c
Maijs Garais <mai-soup@mailfence.com>
2023-10-01 18:38:43 +0100

    get 100% test coverage

--- index.test.js
+++ index.test.js
@@ -14,3 +14,37 @@ it('should throw an error when at least one argument is not a string', () => {
   expect(() => sumStrings(1, getValidNumber())).toThrow()
   expect(() => sumStrings(1, 1)).toThrow()
 })
+
+it('should return the string representation of sum of two numbers when passed two valid strings', () => {
+  const a = getValidNumber()
+  const b = getValidNumber()
+  const result = sumStrings(a, b)
+  expect(result).toBe((Number(a) + Number(b)).toString())
+})
+
+it('should not have leading zeroes when passed at least one number with leading zeroes', () => {
+  const a = '0' + getValidNumber()
+  const b = getValidNumber()
+  const result = sumStrings(a, b)
+  expect(result[0]).not.toBe('0')
+
+  const result2 = sumStrings(b, a)
+  expect(result2[0]).not.toBe('0')
+
+  const c = '0' + getValidNumber()
+  const result3 = sumStrings(a, c)
+  expect(result3[0]).not.toBe('0')
+})
+
+it('should be able to sum two numbers larger than Number.MAX_SAFE_INTEGER', () => {
+  const a = Number.MAX_SAFE_INTEGER.toString() + getValidNumber() + 1
+  const b = getValidNumber()
+  const result = sumStrings(a, b)
+  expect(result).toBe((BigInt(a) + BigInt(b)).toString())
+
+  const result2 = sumStrings(b, a)
+  expect(result2).toBe((BigInt(a) + BigInt(b)).toString())
+
+  const result3 = sumStrings(a, a)
+  expect(result3).toBe((BigInt(a) + BigInt(a)).toString())
+})
